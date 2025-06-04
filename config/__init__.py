031bfe5252fcb24deae21e8578e0db19248ce1fb
Baiheng Xie <874256269@qq.com>
2025-06-04 18:07:33 +0800

    feat(test): tests for preprocess and retrieve

--- config/__init__.py
+++ config/__init__.py
@@ -2,4 +2,10 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
+
+VALID_ENVIRONMENTS = ['dev', 'test', 'prod']
 env = os.getenv('ENV', 'dev')
+
+if env not in VALID_ENVIRONMENTS:
+    raise ValueError(
+        f"Invalid environment: {env}. Must be one of {VALID_ENVIRONMENTS}")
